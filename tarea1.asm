3bac3bfe701f4d59ff6c01a7ee1ebbe42d651bb2
Daniel Serrano Cañas <51306347+dansecan25@users.noreply.github.com>
2024-03-17 23:29:02 -0600

    Add files via upload

--- tarea1.asm
+++ tarea1.asm
@@ -16,6 +16,7 @@ org 100h
 	salir: db '2. Para salir',13,10,'$'
 	bye: db 'Gracias por utilizar CalcuTec',13,10,'$'
 	error: db 'Entrada erronea',13,10,'$'
+	punto: db '.$'
 .code
 	; Label que maneja los mensajes iniciales del programa y espera a que el usuario ingrese una operacion
 	start:
@@ -74,27 +75,56 @@ org 100h
 		int 21h
 		
 		mov cx, 0	; Establece counter en 0
-		call NumEntrada
-		push dx	; Guarda operando en stack
+		xor si,si
+		xor di,di
+		call NumEntrada ;en dx quedara el numero1
+		;si no es ni uno ni dos, no se toca
+		push dx	; Guarda operando en stack (en este punto el ultimo es la parte flotante y el siguiente es la parte entera)
+		
+		
 		mov ah, 9
 		mov dx, offset op2	; Mensaje para ingresar segundo operando
 		int 21h
 		
 		mov cx, 0	; Counter en 0
-		call NumEntrada
-		pop bx	; Guarda operando anterior en bx
-		;convierte en enteros los numeros
-		;mov cx,bx
-		;mov ax,
+		xor si,si
+		xor di,di
+		call NumEntrada;
 		
-		add dx, bx	; Suma los numeros
-		push dx	; Guarda resultado en stack
+        ;en dx anda el segundo numero
+        push dx ;empujo el resulta [float2,int2,float1,int1]
+        pop si ;aqui guardare el float2
+        pop di ;aqui el int2
+        pop dx ;aqui el float1
+        pop cx ;aqui el int1
+        ;proceso de suma
+        add dx,si ;se suman flotantes
+        add cx,di ;se suman enteros
+		;le sumo al cx el primer digito de dx
+		mov si,dx ;float
+		mov di,cx ;int
+		;divido dx entre 100
+		mov bx,100
+		mov ax,si 
+		div bx ;en ax queda el cociente y en dx el residuo, en este caso sera el decimal
+		add bx,di ;en bx ahora tengo la parte entera
+		
+		push dx
+		push bx	; Guarda resultado en stack [int, float]
 		mov ah, 9
 		mov dx, offset result	; Mensaje de resultado
 		int 21h
 		
 		mov cx, 10000 ; Maximo 32 bits
 		pop dx	; Guarda resultado en dx
+		call ResultadoEntero
+		
+		mov ah, 9
+        mov dx,offset punto
+        int 21h
+		
+		mov cx,10
+		pop dx
 		call Resultado
 	
 	
@@ -105,15 +135,23 @@ org 100h
 		int 21h
 		
 		mov cx, 0	; Establece counter en 0
+		xor si,si
+		xor di,di
 		call NumEntrada
+
+		
 		push dx	; Guarda operando en stack
 		mov ah, 9
 		mov dx, offset op2	; Mensaje para ingresar segundo operando
 		int 21h
 		
 		mov cx, 0	; Counter en 0
+		xor si,si
+		xor di,di
 		call NumEntrada	
 		pop bx	; Guarda operando anterior en bx
+		
+		;proceso de multiplicacion
 		mov ax, dx	; Mueve otro operando a ax
 		mul bx	; ax * bx	
 		mov dx, ax	; Mueve el resultado a dx
@@ -134,15 +172,21 @@ org 100h
 		int 21h
 		
 		mov cx, 0	; Establece counter en 0
+		xor si,si
+		xor di,di
 		call NumEntrada
 		push dx	; Guarda operando en stack
 		mov ah, 9
 		mov dx, offset op2	; Mensaje para ingresar segundo operando
 		int 21h
 		
-		mov cx, 0	; Counter en 0
+		mov cx, 0	; Counter en 0 
+		xor si,si
+		xor di,di
 		call NumEntrada
 		pop bx	; Guarda operando anterior en bx
+		
+		;proceso de resta
 		sub bx, dx	; Resta operandos
 		mov dx, bx	; Guarda resultado en dx
 		push dx	; Guarda resultado en stack		
@@ -162,6 +206,8 @@ org 100h
 		int 21h
 		
 		mov cx, 0	; Establece counter en 0
+		xor si,si
+		xor di,di
 		call NumEntrada
 		push dx	; Guarda operando en stack
 		mov ah, 9
@@ -169,8 +215,12 @@ org 100h
 		int 21h
 		
 		mov cx, 0	; Counter en 0
+		xor si,si
+		xor di,di
 		call NumEntrada
 		pop bx	; Guarda operando anterior en bx
+		
+		;proceso de division
 		mov ax, bx	; Mueve operando en ax
 		mov cx, dx	; Mueve otro operando a cx
 		mov dx, 0	; Clear dx
@@ -194,15 +244,16 @@ org 100h
 	NumEntrada:
 		mov ah, 0	; Recibe numero
 		int 16h
-		
 		mov dx, 0	; Inicializa dx
 		mov bx, 1	; Establece 1 para multiplicacion
 		cmp al, 0Dh	; Si presiona enter
 		je CreaNum
-		cmp al,2Eh
-		jne noFlot
-		je hayFlot
+		jne revisarFlotante
 		
+	revisarFlotante: ;revisa si se encuentra un punto flotante
+	    cmp al,2Eh ;si es igual a .
+		jne noFlotMostrar ;brinca a mostrar el numero si no es .
+		je hayFlot ;cuando topa con el flotante, guarda la parte entera en el stack 	
 
 	; Label que se encarga de guardar operando en dx
 	CreaNum:
@@ -220,27 +271,82 @@ org 100h
 		dec cx	; Decrementa cuenta decimal
 		cmp cx, 0
 		jne CreaNum	; if cx != 0, todavia quedan digitos del num
-		cmp cx,2Eh
-		je hayFlot ;si el digito es un punto, hay flotante
 		ret ; Else regrese a label anterior
 		
-    ;si no hay punto, convierte el numero a ascii
-    noFlot:
+	; Label que se encarga de guardar operando en dx
+	CreaNumEntero:
+		pop ax	; Guarda ultimo digito en ax
+		push dx	; Guarda digito en stack		 
+		mul bx	; ax * bx
+		pop dx	; Guarda digito en dx
+		add dx, ax	; Guarda digito en dx
+		mov ax, bx	; Guarda bx en ax
+		mov bx, 10	; Para agregar digito al numero
+		push dx	; Guarda digito en stack
+		mul bx	; Multiplica ax por 10
+		pop dx	; Guarda digito en dx
+		mov bx, ax	; Mueve resultado de multiplicacion en bx
+		dec cx	; Decrementa cuenta decimal
+		cmp cx, 0
+		jne CreaNumEntero	; if cx != 0, todavia quedan digitos del num
+		je continuarHayFlot ; Else regresa a hayFlot
+		
+    ;si no es un punto, convierte el numero a ascii y lo muestra
+    noFlotMostrar:
         sub ax, 30h ; ASCII a numero decimal
 		call MostrarNum
-		mov ah, 0
+		mov ah, 0 
 		push ax	; Guarda numero en stack
 		inc cx	; Contador + 1
 		jmp NumEntrada
-	hayFlot:
-	    ;guardar cantidad de digitos despues del punto
+    noFlot: ;si no 
+        sub ax, 30h ; ASCII a numero decimal
+		call MostrarNum
+		mov ah, 0 
+		push ax	; Guarda numero en stack
+		inc cx	; Contador + 1
+		jmp NumEntrada
+		
+    ;si hay punto, lo muestra pero no lo guarda en el stack
+    ;e indica en di que se encontro un punto
+	hayFlot: ;en hay flot, se almacenara en el stack la parte entera del numero
+	    ;guardar cantidad de digitos despues del punto 
 	    call MostrarPunto
 	    mov ah,0
+	    inc di ;di+=1, binario, 0 si no ha aparecido un punto, 1 si aparecio
+	    ;-----
+	    jmp CreaNumEntero
+	    continuarHayFlot:
+	    push dx
+	    ;resetea valores para llamar a NumEntrada de nuevo para guardar la parte flotante
+	    mov cx,0
+
+	    ;crear num para la parte entera
 	    jmp NumEntrada
+
+	    
+	      
+	    
 	; Label que se encarga de procesar el resultado de la operacion
 	
 	
 	Resultado:
+		mov ax, dx	; Mueve resultado a ax
+		mov dx, 0	
+		div cx	; cx / ax para obtener primer digito
+		call MostrarNum
+		mov bx, dx	; Mueve resultado a bx
+		mov dx, 0	; Reset dx
+		mov ax, cx	; Mueve 10 a ax
+		mov cx, 10
+		div cx	; Guarda en ax division de 10 / 10
+		mov dx, bx	; Guarda resultado en dx
+		mov cx, ax	; Guarda division en cx
+		cmp ax, 0	
+		jne Resultado	; Si todavia quedan digitos, repetir
+		jmp exit
+    
+    ResultadoEntero:
 		mov ax, dx	; Mueve resultado a ax
 		mov dx, 0	
 		div cx	; cx / ax para obtener primer digito
@@ -254,7 +360,7 @@ org 100h
 		mov cx, ax	; Guarda division en cx
 		cmp ax, 0	
 		jne Resultado	; Si todavia quedan digitos, repetir
-		jmp exit
+		ret
 
 	; Label que se encarga de mostrar el numero en el registro ax
 	MostrarNum:
